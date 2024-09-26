099e8097e9e2fe4d7105fd682b828cbec098baf8
Захаров Илья <zaxaroov2004@yandex.ru>
2024-09-26 18:23:38 +0700

    Меню

--- app.py
+++ app.py
@@ -1,8 +1,41 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/menu")
+@app.route("/")
+@app.route("/index")
 def start():
+
+    return """
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Захаров Илья Максимович, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+        
+        <div>
+            <ol>
+                <li>
+                    <a href="/lab1">Лабораторная работа 1</a>
+                </li>
+            
+            </ol>
+        </div>
+
+        <footer>
+            &copy; Захаров Илья, ФБИ-24, 3 курс, 2024 
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route("/lab1")
+def lab1():
     return """
 <!DOCTYPE html>
 <html lang="ru">
@@ -15,9 +48,12 @@ def start():
         <header>
             НГТУ, ФБ, Лабораторная работа 1
         </header>
+        <p>Flask — фреймворк для создания веб-приложений на языке программирования Python, 
+        использующий набор инструментов Werkzeug, а также шаблонизатор Jinja2. Относится к 
+        категории так называемых микрофреймворков — минималистичных каркасов веб-приложений, 
+        сознательно предоставляющих лишь самые базовые возможности.</p>
         
-        <h1>web-сервер на flask</h1>
-
+        
         <footer>
             &copy; Захаров Илья, ФБИ-24, 3 курс, 2024 
         </footer>
