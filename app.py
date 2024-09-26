17a63d5e7d99df9efe688b3a8b673052c1c80ba1
Захаров Илья <zaxaroov2004@yandex.ru>
2024-09-26 18:05:24 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/menu")
 def start():
-    return "Web-сервер на Flask"
-    
+    return """
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Захаров Илья Максимович, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+        
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Захаров Илья, ФБИ-24, 3 курс, 2024 
+        </footer>
+    </body>
+</html>
+"""
